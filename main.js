6ef28c087dff13f2daf8a513ea1950470bacf89f
abdunabiabduvakhobovv <abdunabiabduvakhobov60@gmail.com>
2025-02-12 13:53:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let name = prompt("string typedagi malumotni kiriting")
+console.log(name );
+let surname = prompt("number typedagi malumotni kiriting")
+console.log( surname);
+let boolean = prompt("boolean typedagi malumotni kiriting")
+console.log(boolean);
+alert("hammasini togri yosganingizga ishonchingiz komilmi?")
+alert("siz kiritgan " + name + " \nsiz kiritgan " + surname + " \nsiz kiritgan " + boolean  )
+
+    
